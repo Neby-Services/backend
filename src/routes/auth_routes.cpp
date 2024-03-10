8d2080cef21efabbce52dce19c0e7742625a8963
JayexDesigns <joaquintouron@gmail.com>
2024-03-10 21:29:09 +0100

    fix: minor fixes

--- src/routes/auth_routes.cpp
+++ src/routes/auth_routes.cpp
@@ -1,8 +1,7 @@
 #include "auth_routes.h"
 
 void initialize_auth_routes(crow::SimpleApp& app, pqxx::connection& db) {
-	CROW_ROUTE(app, "/api/auth/register")
-	([&db](const crow::request& req, crow::response& res) {
+	CROW_ROUTE(app, "/api/auth/register").methods("POST"_method)([&db](const crow::request& req, crow::response& res) {
 		UserController::create_user(db, req, res);
 	});
 }
