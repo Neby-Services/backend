7d057be852ac2194af04c7c18f6a16097ae43e25
JayexDesigns <joaquintouron@gmail.com>
2024-03-11 09:19:27 +0100

    task #60: connect register route, controller and model

--- src/controllers/auth_controller.cpp
+++ src/controllers/auth_controller.cpp
@@ -1,17 +1,17 @@
 #include "controllers/auth_controller.h"
 
 void AuthController::register_user(pqxx::connection& db, const crow::request& req, crow::response& res) {
-	/* 	res.code = 200;
-		auto y = crow::json::load(req.body);
+	crow::json::rvalue body = crow::json::load(req.body);
 
-		crow::json::wvalue x({{"message", "Hello, World!"}});
-		x["message2"] = "Hello!";
-		res.write(x.dump());
-		res.end(); */
+	std::string password = body["password"].s();
+	std::string username = body["username"].s();
+	std::string email = body["email"].s();
+	std::string type = body["type"].s();
 
-	auto y = crow::json::load(req.body);
-	crow::json::wvalue x(y);
+	UserModel user = UserModel::create_user(db, password, email, username, "", 0, type);
 
-	res.write(x.dump());
+	crow::json::wvalue data({{"id", user.getId()}});
+	res.code = 200;
+	res.write(data.dump());
 	res.end();
 }
