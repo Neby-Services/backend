4a08888f704b7bbc021f041c2e040c44f58c9f3c
leobelab <leobenitezzz77@gmail.com>
2024-03-12 16:55:29 +0100

    task #71: get_users created

--- include/models/user_model.h
+++ include/models/user_model.h
@@ -1,11 +1,11 @@
 #include <pqxx/pqxx>
 #include <string>
+#include <vector>
 
 class UserModel {
 	private:
 	std::string _id;
-	std::string _password;
-	std::string _mail;
+	std::string _email;
 	std::string _username;
 	std::string _image_url;
 	int _balance;
@@ -13,11 +13,17 @@ class UserModel {
 
 	public:
 	UserModel(std::string id);
+	UserModel(std::string id, std::string email, std::string username, std::string image_url, int balance, std::string type);
 
 	std::string getId();
+	std::string getEmail();
+	std::string getUsername();
+	std::string getImageUrl();
+	int getBalance();
+	std::string getType();
 
 	static UserModel create_user(pqxx::connection& db, std::string password, std::string email, std::string username, std::string image_url, int balance, std::string type);
 	static bool user_exist(pqxx::connection& db, std::string email);
 	static bool username_exist(pqxx::connection& db, std::string username);
-
+	static std::vector<UserModel> get_users(pqxx::connection& db);
 };
