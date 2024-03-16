fa2e77c39195544a3537d854a8ff8ee51d6c49f9
JayexDesigns <joaquintouron@gmail.com>
2024-03-16 10:34:42 +0100

    task #80: removed test routes, controller and model

--- include/routes/test_routes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <pqxx/pqxx>
-
-#include "controllers/test_controller.h"
-#include "crow.h"
-#include "utils/common.h"
-
-void initialize_test_routes(NebyApp& app, pqxx::connection& db);
