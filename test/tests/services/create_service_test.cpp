5769c869361121b88e2a299f2322ba3bf0c1de21
David Baque <baquedavid0@gmail.com>
2024-04-03 22:43:41 +0200

    base

---
+++ test/tests/services/create_service_test.cpp
@@ -0,0 +1,10 @@
+#include <cpr/cpr.h>
+#include <gtest/gtest.h>
+
+#include <cstdlib>	// Para std::getenv
+#include <nlohmann/json.hpp>
+#include <pqxx/pqxx>
+#include <string>
+#include <vector>
+
+#include "../common.h"
